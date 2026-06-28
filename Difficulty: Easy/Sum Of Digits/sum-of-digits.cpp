3414720b34ddae3118fa4450ef5e7e5c5cfbc506
SouraDT <157200969+iamsoura005@users.noreply.github.com>
2026-06-29 01:07:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int sumOfDigits(int n) {
+        // code here
+        int sum =0;
+        while(n>0) {
+            int digit = n%10;
+            sum = sum+digit;
+            n=n/10;
+        }
+        return sum;
+    }
+};
