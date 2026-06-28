36579a9647bc95f1797b499cd56eb0e64a20b4da
SouraDT <157200969+iamsoura005@users.noreply.github.com>
2026-06-28 21:13:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -3,16 +3,16 @@ class Solution {
     vector<int> lcmAndGcd(int a, int b) {
         // code here
         int x =a;
-        int y =b;
-        //euclid algo GCD
-        while(b!=0){ //repeat untill remainder
-        int rem= a%b;
-        a = b;
+        int y = b; //storing the number
+        //euclid to find gcd
+        while (b!=0) {  //repeat untill second number becomes 0
+        int rem= a%b; //rem
+        a=b; //move smaller to a 
         b= rem;
-            
-        }
-        int gcd = a;
-        int lcm=(x*y) / gcd;
-        return {lcm,gcd};
+        
+    }
+    int gcd =a; 
+    int lcm= (x*y) / gcd;
+    return {lcm,gcd};
     }
 };
