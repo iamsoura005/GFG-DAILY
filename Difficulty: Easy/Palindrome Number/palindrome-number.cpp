60fcf28f3a861c46fa03be200499485ab340dbff
SouraDT <157200969+iamsoura005@users.noreply.github.com>
2026-07-06 20:20:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Number/palindrome-number.cpp
+++ Difficulty: Easy/Palindrome Number/palindrome-number.cpp
@@ -1,12 +1,25 @@
 class Solution {
-public:
+  public:
     bool isPalindrome(int n) {
-        // Take the absolute value to ignore the negative sign
-        string s = to_string(abs(n)); 
-        
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        
-        return s == rev;
+        // Take the absolute value to ignore the negative sign as per problem requirements
+        n = abs(n);
+
+        // Convert number into string
+        string s = to_string(n);
+
+        // Two pointers
+        int left = 0;
+        int right = s.length() - 1;
+
+        // Compare characters from both ends
+        while (left < right) {
+            if (s[left] != s[right]) // If characters don't match
+                return false;
+
+            left++;  // Move left pointer forward
+            right--; // Move right pointer backward
+        }
+
+        return true; // All characters matched
     }
 };
