841075154fe1fd5a8f46b6a9ca708b9ceb79aa88
SouraDT <157200969+iamsoura005@users.noreply.github.com>
2026-07-07 05:45:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -3,24 +3,18 @@ class Solution {
     int convertFive(int n) {
         // code here
         //special case
-        if(n == 0) 
+        if(n==0) 
             return 5;
-        int newNum=0;
-        //build reversed modified no
-        while(n>0) {
-            int digit= n%10; //extract last digit
-            if(digit == 0) 
-            digit =5;
-            newNum= newNum*10 + digit;
-            n=n/10;
+        //convert to string
+        string s = to_string(n);
+        for(int i=0; i< s.length(); i++)
+        {
+            //replace 0 with 5
+            if(s[i] == '0'){
+                s[i]= '5';
+            }
         }
-        //reverse again
-        int answer = 0;
-        while(newNum > 0) {
-            int digit = newNum %10;
-            answer = answer *10 +digit;
-            newNum = newNum /10;
-        }
-        return answer;
+        return stoi(s);
+        
     }
 };
