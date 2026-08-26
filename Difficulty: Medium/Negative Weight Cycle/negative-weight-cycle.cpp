2fe31e08b95c4ea53bd950f6416fe6e7bb2286ae
SouraDT <157200969+iamsoura005@users.noreply.github.com>
2026-08-26 12:43:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Negative Weight Cycle/negative-weight-cycle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isNegativeWeightCycle(int V, vector<vector<int>>& edges) {
+        vector<int> dist(V, 0);
+
+        for (int i = 0; i < V - 1; ++i) {
+            for (const auto& edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                int weight = edge[2];
+                if (dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                }
+            }
+        }
+
+        for (const auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+            if (dist[u] + weight < dist[v]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
